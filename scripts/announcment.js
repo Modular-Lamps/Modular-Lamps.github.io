5c5f830d75be1cbc71c3bd36dbdb9d6ff0a0cdcc
Bajojajo-xD <81306360+Bajojajo-xD@users.noreply.github.com>
2022-12-03 22:08:50 +0100

    ♻️ Add docs site

--- scripts/announcment.js
+++ scripts/announcment.js
@@ -1,10 +1,22 @@
-var text = 
+var globalAnnouncment = 
 
 //HERE WRITE ANNOUNCMENT
-'<i class="fa-solid fa-wifi"></i> Under development'
+'<i class="fa-solid fa-bell"></i> In beta version'
 
 
 
+//HERE WRITE ADDITIONAL ANNOUNCMENTS FOR SPECIFIED FILES
+var mainAnnouncment = '',
+    aboutAnnouncment = '',
+    docsAnnouncment = '<i class="fa-solid fa-truck-fast"></i> Docs not ready, working on it'
 
+
+var fileName = location.href.split("/").slice(-1), text;
 var announcment = document.getElementById("announcment")
+console.log(fileName[0])
+if(fileName[0] == 'docs.html') text = docsAnnouncment
+else if (fileName[0] == 'about.html') text = aboutAnnouncment
+else if (fileName[0] == 'index.html') text = mainAnnouncment
+text ? (globalAnnouncment ? text = text + '<br />' + globalAnnouncment : text = text) : (globalAnnouncment ? text = globalAnnouncment : text = '')
+
 announcment.innerHTML = text
