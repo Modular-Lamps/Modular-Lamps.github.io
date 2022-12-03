bb40ac445e042566630fabbc513dd322efce2dac
Bajojajo-xD <81306360+Bajojajo-xD@users.noreply.github.com>
2022-12-03 17:02:36 +0100

    🛠️ Cleanup code and change announcment

--- docs/scripts/announcment.js
+++ docs/scripts/announcment.js
@@ -1,37 +1,10 @@
-var announcmentEl = document.getElementById("announcment")
-var announcmentBg = document.getElementById("announcment-bg")
-var width = window.matchMedia("(max-width: 700px)")
+var text = 
 
-function checkWidth() {
-  if(announcmentEl.style.opacity == '1') {
-    if(!width.matches) {
-      announcmentBg.style.paddingRight = "400px"
-      announcmentBg.style.paddingBottom = "36px"
-    }
-    else {
-      announcmentBg.style.paddingRight = "240px"
-      announcmentBg.style.paddingBottom = "34px"
-    }
-  }
-}
-checkWidth() // Call listener function at run time
-width.addListener(checkWidth) // Attach listener function on state changes
+//HERE WRITE ANNOUNCMENT
+'<i class="fa-solid fa-wifi"></i> Under development'
 
-function announcment() {
-  if(announcmentEl.style.opacity == '0' || !announcmentEl.style.opacity) {
-    announcmentBg.style.padding = "0px"
-    !width.matches ? announcmentBg.style.paddingRight = "400px" :   announcmentBg.style.paddingRight = "240px"
-    !width.matches ? announcmentBg.style.paddingBottom = "36px" :   announcmentBg.style.paddingBottom = "34px"
-    announcmentBg.style.backgroundColor = 'rgb(255, 255, 0)'
-    setTimeout(() => {
-      announcmentEl.style.opacity = "1"
-    }, 500);
-  }
-  else {
-    announcmentEl.style.opacity = "0"
-    setTimeout(() => {
-      announcmentBg.style.padding = "10px"
-      announcmentBg.style.backgroundColor = 'rgb(255, 0, 0)'
-    }, 500);
-  }
-}
+
+
+
+var announcment = document.getElementById("announcment")
+announcment.innerHTML = text
